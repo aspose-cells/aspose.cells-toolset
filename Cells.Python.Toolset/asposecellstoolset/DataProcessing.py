afb6c1962b8694af426705ec0b080b79a8eeb054
roy.wang <roy.wang@aspose.com>
2024-03-02 06:02:01 +0800

    2024-03-02 : Synchronize codes to the origin repository.

--- Cells.Python.Toolset/asposecellstoolset/DataProcessing.py
+++ Cells.Python.Toolset/asposecellstoolset/DataProcessing.py
@@ -1,5 +1,6 @@
-from tkinter import ALL
+from xmlrpc.client import Boolean
 from aspose.cells import Workbook
+from aspose.cells import Worksheet
 from aspose.cells import FileFormatType
 from aspose.cells import CellValueType
 
@@ -9,15 +10,15 @@ class DataProcessing(object):
 
         pass
      
-    def data_cleansing(self , workbook : Workbook,  **kwargs):
-        
+    def data_cleansing(self , workbook : Workbook, need_fill_data : bool , **kwargs):        
         self.data_deduplication(workbook ,kwargs)
-        self.data_fill(workbook ,kwargs)
+        if need_fill_data :
+            self.data_fill(workbook ,kwargs)
         
         pass
     
     def data_deduplication(self , workbook : Workbook,**kwargs):        
-        entireSheet = workbook.file_format() in [ FileFormatType.CSV() , FileFormatType.TSV() , FileFormatType.HTML() ,FileFormatType.M_HTML()]
+        entireSheet = workbook.file_format in [ FileFormatType.CSV , FileFormatType.TSV , FileFormatType.HTML ,FileFormatType.M_HTML ]
         if entireSheet :
             for sheet in workbook.worksheets:
                 sheet.cells.remove_duplicates()
@@ -26,26 +27,73 @@ class DataProcessing(object):
                 worksheet = workbook.worksheets.get(namerange.worksheet.name() );
                 worksheet.cells.remove_duplicates(namerange.first_row, namerange.first_column, namerange.first_row + namerange.row_count - 1, namerange.first_column + namerange.column_count - 1);
             for sheet in workbook.worksheets:
-                for table in sheet.list_objects:
-                    sheet.cells.remove_duplicates(table.start_row, table.start_column, table.end_row, table.end_column)
-                    for row in range(table.end_row , table.start_row):
-                        needBreak = False
-                        for column in range(table.start_column , table.end_column):
-                            cell = sheet.cells.get(row , column)
-                            if cell.type == CellValueType.IsNull :
-                                continue
-                            else :
-                                needBreak = True
+                if sheet.list_objects.count > 0 :
+                    for table in sheet.list_objects:
+                        sheet.cells.remove_duplicates(table.start_row, table.start_column, table.end_row, table.end_column)
+                        for row in range(table.end_row , table.start_row):
+                            needBreak = False
+                            for column in range(table.start_column , table.end_column):
+                                cell = sheet.cells.get(row , column)
+                                if cell.type == CellValueType.IsNull :
+                                    continue
+                                else :
+                                    needBreak = True
+                                    break
+                            if needBreak :
+                                table.resize(table.start_row, table.start_column, row, table.end_column, table.show_header_row)
                                 break
-                        if needBreak :
-                            table.resize(table.start_row, table.start_column, row, table.end_column, table.show_header_row)
-                            break
-        
+                else:
+                    sheet.cells.remove_duplicates()
         pass
     
-    def data_fill(self , workbook : Workbook,**kwargs):
-        
+    def data_fill(self , workbook : Workbook, **kwargs):
+        entireSheet = workbook.file_format in [ FileFormatType.CSV , FileFormatType.TSV , FileFormatType.HTML ,FileFormatType.M_HTML ]
+        if entireSheet :
+            for worksheet in workbook.worksheets:
+                for column_index in range ( 0, worksheet.cells.max_data_column):
+                    for row_index in range( 0 , worksheet.cells.max_data_row):                    
+                        cell = worksheet.cells.get( row_index , column_index )
+                        if cell.type == CellValueType.IS_NULL :
+                            cell.put_value(0)                        
+        else:
+            for worksheet in workbook.worksheets:
+                if worksheet.list_objects.count > 0 :
+                    for table in worksheet.list_objects:
+                        for column_index in range ( table.start_column , table.end_column ):
+                            for row_index in range(table.end_row , table.start_row):
+                                cell = worksheet.cells.get( row_index , column_index )
+                                if cell.type == CellValueType.IsNull :
+                                    cell.put_value(0)    
+                else:
+                    for column_index in range ( 0, worksheet.cells.max_data_column):
+                        for row_index in range( 0 , worksheet.cells.max_data_row):                    
+                            cell = worksheet.cells.get( row_index , column_index )
+                            if cell.type == CellValueType.IS_NULL :
+                                cell.put_value(0)       
         pass
+    
+    def get_column_main_data_type(self, worksheet :Worksheet , column_index : int ,**kwargs ):
+        cell_type_set ={}
+        
+        for row_index in range(0, worksheet.cells.max_data_row):
+            cell = worksheet.cells.get( row_index , column_index )
+            value_type = cell.type 
+            if value_type in [ CellValueType.IS_NULL ,CellValueType.IS_NULL , CellValueType.IS_NULL ] :
+                continue
+            else:
+                if cell.type in cell_type_set :
+                    count =  cell_type_set[cell.type] 
+                    cell_type_set[cell.type] = count + 1
+                else:
+                    cell_type_set[cell.type] =  1
+        count = 0
+        last_cell_value_type =  CellValueType.IS_NULL
+        for cell_value_type in cell_type_set:           
+            if count < cell_value_type.value :
+                count = cell_value_type.value
+                last_cell_value_type = cell_value_type.key
+        return last_cell_value_type
+        pass 
 
 
                 
