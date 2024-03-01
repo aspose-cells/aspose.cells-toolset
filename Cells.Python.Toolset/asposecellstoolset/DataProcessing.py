7de7fcaf17c69b43664f7d8c78a69358bf164409
roy.wang <roy.wang@aspose.com>
2024-03-01 10:04:43 +0800

    2024-03-01 : Synchronize codes to the origin repository.

--- Cells.Python.Toolset/asposecellstoolset/DataProcessing.py
+++ Cells.Python.Toolset/asposecellstoolset/DataProcessing.py
@@ -1,5 +1,7 @@
+from tkinter import ALL
 from aspose.cells import Workbook
-from aspose.cells import FileFormat
+from aspose.cells import FileFormatType
+
 
 class DataProcessing(object):
     
@@ -8,7 +10,20 @@ class DataProcessing(object):
         pass
      
     def data_cleansing(self , workbook : Workbook,  **kwargs):
-        for  sheet in workbook.worksheets:
-            sheet.cells.RemoveDuplicates()
+        
+
             
         pass
+    
+    def data_deduplication(self , workbook : Workbook,**kwargs):
+        
+        entireSheet = workbook.file_format() in [ FileFormatType.CSV() , FileFormatType.TSV() , FileFormatType.HTML() ,FileFormatType.M_HTML()]
+         
+        for sheet in workbook.worksheets:
+            if entireSheet :    
+                sheet.cells.remove_duplicates()
+            else:
+                for namerange in workbook.worksheets.get_named_ranges() :
+                    worksheet = workbook.worksheets.get(namerange.worksheet().name() );
+                    worksheet.cells.remove_duplicates(namerange.FirstRow, namerange.FirstColumn, namerange.FirstRow + namerange.RowCount - 1, namerange.FirstColumn + namerange.ColumnCount - 1);
+            
