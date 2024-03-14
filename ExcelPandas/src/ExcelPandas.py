5f80ca3f696f391c269e2ffc952404188040506e
roy.wang <roy.wang@aspose.com>
2024-03-14 21:00:11 +0800

    2024-03-14 : Synchronize codes to the origin repository.

--- ExcelPandas/src/ExcelPandas.py
+++ ExcelPandas/src/ExcelPandas.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 import pandas as pd
 import re
+import io
 from aspose.cells import Workbook
 from aspose.cells import Worksheet
 from aspose.cells import Cells
@@ -22,8 +23,9 @@ class ExcelPandas(object):
         :param int list_object_index: The list object index indicates the position in the spreadsheet. If the worksheet index is None, the default worksheet index is the active worksheet index. (optional)
         :param int pivot_table_index: The worksheet index indicates the position in the spreadsheet. If the worksheet index is None, the default worksheet index is the active worksheet index. (optional)
         :param int chart_index: The worksheet index indicates the position in the spreadsheet. If the worksheet index is None, the default worksheet index is the active worksheet index. (optional)
-        :param int range_name: The worksheet index indicates the position in the spreadsheet. If the worksheet index is None, the default worksheet index is the active worksheet index. (optional)
-        :return list data: 
+        :param int cell_area: The worksheet cell_area indicates the position in the spreadsheet. If the worksheet index is None, the default worksheet index is the active worksheet index. (optional)
+        :param int name_text: The workbook name indicates the position in the spreadsheet. If the worksheet index is None, the default worksheet index is the active worksheet index. (optional)
+        :return DataFrame: 
         """                
     def read_spreadsheet( self , path: str , **kwargs )-> pd.DataFrame:
         workbook = Workbook(path)
@@ -42,10 +44,13 @@ class ExcelPandas(object):
         chart_index = None
         if kwargs.get("chart_index") is not None:
             chart_index = kwargs.get("chart_index")
-        range_name = None
-        if kwargs.get("range_name") is not None:
-            range_name = kwargs.get("range_name")        
-
+        cell_area = None
+        if kwargs.get("cell_area") is not None:
+            cell_area = kwargs.get("cell_area")        
+        name_text = None
+        if kwargs.get("name_text") is not None:
+            name_text = kwargs.get("name_text") 
+            
         if list_object_index is not None:
             worksheet = workbook.worksheets[sheet_index]
             table  = worksheet.list_objects[list_object_index]
@@ -60,14 +65,45 @@ class ExcelPandas(object):
         if chart_index is not None:
             return self.__get_chart_data_to_dataframe( workbook,sheet_index,chart_index )
 
+        if cell_area is not None:
+            tuple_cell_area = self.__parse_cell_area(cell_area)
+            cells = workbook.worksheets[sheet_index].cells
+            has_table_header = self.__has_table_header(cells,tuple_cell_area[0],tuple_cell_area[1],tuple_cell_area[2],tuple_cell_area[3])
+            return self.__get_data_to_dataframe(cells,tuple_cell_area[0],tuple_cell_area[1],tuple_cell_area[2],tuple_cell_area[3],has_table_header,False )
+        
+        if name_text is not None:
+            name_range = workbook.worksheets.get_range_by_name(name_text)
+            
+            cells = name_range.worksheet.cells
+            begin_row_index = name_range.first_row
+            begin_column_index = name_range.first_column
+            end_row_index = name_range.first_row + name_range.row_count -1
+            end_column_index = name_range.first_column + name_range.column_count -1            
+            has_table_header = self.__has_table_header(cells,begin_row_index,begin_column_index,end_row_index,end_column_index)
+            return self.__get_data_to_dataframe(cells,begin_row_index,begin_column_index,end_row_index,end_column_index,has_table_header,False )
+        
         cells = workbook.worksheets[sheet_index].cells
-        return self.__get_data_to_dataframe(cells,cells.min_data_row,cells.min_data_column,cells.max_data_row,cells.max_data_column,False,False )
+        has_table_header = self.__has_table_header(cells,cells.min_data_row,cells.min_data_column,cells.max_data_row,cells.max_data_column)
+        return self.__get_data_to_dataframe(cells,cells.min_data_row,cells.min_data_column,cells.max_data_row,cells.max_data_column,has_table_header,False )
 
         pass
     
 
-
-
+        """
+        read data form spreadsheet which is include of Excel, cvs, txt, ods, iCalc and so on.
+        :param str path: If the file exists, it will be appended to the file. If the file does not exist, a new file is created and written.  (required)
+        :param int sheet_index: The worksheet index indicates the position in the spreadsheet. (optional)
+        :param int list_object_index: The list object index indicates the position in the spreadsheet. If the worksheet index is None, the default worksheet index is the active worksheet index. (optional)
+        :param int pivot_table_index: The worksheet index indicates the position in the spreadsheet. If the worksheet index is None, the default worksheet index is the active worksheet index. (optional)
+        :param int chart_index: The worksheet index indicates the position in the spreadsheet. If the worksheet index is None, the default worksheet index is the active worksheet index. (optional)
+        :param int cell_area: The worksheet cell_area indicates the position in the spreadsheet. If the worksheet index is None, the default worksheet index is the active worksheet index. (optional)
+        :param int name_text: The workbook name indicates the position in the spreadsheet. If the worksheet index is None, the default worksheet index is the active worksheet index. (optional)
+        :return  
+        """  
+        def write_spreadsheet( self , path: str,  **kwargs ) :
+        
+            pass
+    
     def __get_data_to_dataframe( self , cells : Cells , begin_row_index : int , begin_column_index : int , end_row_index : int , end_column_index : int , has_header: bool, has_total : bool)->pd.DataFrame:        
         column_title_list =[]
         row_index = 0
@@ -102,7 +138,18 @@ class ExcelPandas(object):
             position = position + 1
             
         return pd.DataFrame(data)
-        
+    def __has_table_header(self, cells: Cells, begin_row_index :int, begin_column_index:int, end_row_index :int, end_column_index:int ):
+        has_header = True
+        for column_index in range(begin_column_index , end_column_index +1) :
+            cell = cells.get(begin_row_index,column_index)
+            if cell.type != CellValueType.IS_STRING :
+                has_header = False
+                break
+            sen_cell = cells.get(begin_row_index+1,column_index)
+            if cell.type != sen_cell.type :
+                break
+        return has_header
+        pass        
     def __get_chart_data_to_dataframe( self , workbook : Workbook, sheet_index : int , chart_index : int )->pd.DataFrame:
         chart = workbook.worksheets[sheet_index].charts[chart_index]
         data = {}
@@ -122,13 +169,13 @@ class ExcelPandas(object):
             
         data[xName] = column_data
         yNames = []
-        for index in range( 0, chart.n_series.count):
-            values = self.__parse_data_source( chart.n_series.get(index).values)
+        for index in range( 0,len (chart.n_series)):
+            values = self.__parse_data_source( chart.n_series[index].values)
             values_data = []
             for row_index in range (values[1],values[3] +1):
-                values_data.append(cells.get(row_index , column_index).value)
-            data[chart.n_series.get(index).display_name] = values_data
-            yNames.append(chart.n_series.get(index).display_name)
+                values_data.append(cells.get(row_index , column_index + index + 1).value)
+            data[chart.n_series[index].display_name] = values_data
+            yNames.append(chart.n_series[index].display_name)
         return pd.DataFrame(data)
             
     def __parse_data_source( self , value : str):        
@@ -136,5 +183,10 @@ class ExcelPandas(object):
         if matchObj == None :
             return None
         
-        return (matchObj.group(1) , matchObj.group(3) - 1 , CellsHelper.column_name_to_index (matchObj.group(2)),  matchObj.group(5) -1 ,  CellsHelper.column_name_to_index (matchObj.group(4)) )
+        return (matchObj.group(1) ,int( matchObj.group(3)) - 1 ,int( CellsHelper.column_name_to_index (matchObj.group(2))), int( matchObj.group(5)) -1 , int(  CellsHelper.column_name_to_index (matchObj.group(4))) )
             
+    def __parse_cell_area( self , value : str):        
+        matchObj = re.match( r'^([a-zA-Z]*)(\d+):([a-zA-Z]*)(\d+)', value, re.M|re.I)
+        if matchObj == None :
+            return None
+        return (int( matchObj.group(2)) - 1 ,int( CellsHelper.column_name_to_index (matchObj.group(1))), int( matchObj.group(4)) -1 , int(  CellsHelper.column_name_to_index (matchObj.group(3))) )
--- ExcelPandas/test/test_excel_pandas.py
+++ ExcelPandas/test/test_excel_pandas.py
@@ -23,21 +23,46 @@ class TestExcelPandas( unittest.TestCase):
     def tearDown(self):
         pass
     
-    
+    def test_read_spreadsheet_csv(self):
+        excelPandas = ExcelPandas()
+        df = excelPandas.read_spreadsheet("D:\cells-toolset\TestData\Input\BookTableData.csv")    
+        assert df.shape == (20,5)
+        # print(df)
+        pass
+    def test_read_spreadsheet_xlsx(self):
+        excelPandas = ExcelPandas()
+        df = excelPandas.read_spreadsheet("D:\cells-toolset\TestData\Input\BookData.xlsx")    
+        assert df.shape == (20,5)
+        # print(df)
+        pass
+    def test_read_spreadsheet_name(self):
+        excelPandas = ExcelPandas()
+        df = excelPandas.read_spreadsheet("D:\cells-toolset\TestData\Input\BookChartData.xlsx" , name_text = "RangeData")    
+        assert df.shape == (20,5)
+
+        pass    
     def test_read_spreadsheet_listobject(self):
         excelPandas = ExcelPandas()
-        df = excelPandas.read_spreadsheet("D:\cells-toolset\TestData\Input\BookChartData.xlsx", sheet_index = 0, list_object_index = 0)        
+        df = excelPandas.read_spreadsheet("D:\cells-toolset\TestData\Input\BookChartData.xlsx", sheet_index = 0, list_object_index = 0)      
+        assert df.shape == (20,5)
+        # print(df)
         pass
-    
+    def test_read_spreadsheet_cellarea(self):
+        excelPandas = ExcelPandas()
+        df = excelPandas.read_spreadsheet("D:\cells-toolset\TestData\Input\BookChartData.xlsx", sheet_index = 2 , cell_area="D15:H35")
+        assert df.shape == (20,5)
+        # print(df)
+        pass      
     def test_read_spreadsheet_sheet(self):
         excelPandas = ExcelPandas()
         df = excelPandas.read_spreadsheet("D:\cells-toolset\TestData\Input\BookChartData.xlsx", sheet_index = 2)
+        assert df.shape == (20,5)
         pass   
     
     def test_read_spreadsheet_chart(self):
         excelPandas = ExcelPandas()
         df = excelPandas.read_spreadsheet("D:\cells-toolset\TestData\Input\BookChartData.xlsx", sheet_index = 1,chart_index = 0)
-        print(df)
+        assert df.shape == (20,4)
         pass 
         
 if __name__ == '__main__':
