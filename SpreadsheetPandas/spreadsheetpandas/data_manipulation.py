e42f077fc33550de4fad6f40937e44c05725330e
roy.wang <roy.wang@aspose.com>
2024-04-18 21:34:16 +0800

    2024-04-18 : Synchronize codes to the origin repository.

--- SpreadsheetPandas/spreadsheetpandas/data_manipulation.py
+++ SpreadsheetPandas/spreadsheetpandas/data_manipulation.py
@@ -15,13 +15,63 @@ import numpy as np
 import pandas as pd
 
 ## cells object to python object
-def pivot_column( table : ListObject ,map_column_name : str , column_names_to_count : list ) ->list:
+def pivot_column( table: ListObject , pivot_column: str , value_column:str , aggregation: str) ->list :
     """
     List Object 
     :param ListObject table:  (required)
-    :param str map_column_name:  (required)
-    :param list column_names_to_count:  (required)
+    :param str pivot_column:  (required)
+    :param str value_column:  (required)
+    :param str aggregation:  (required)
     :return list: 
     """
+
+    cells = table.data_range.worksheet.cells
+    pivot_column_index = 0
+    value_column_index = 0
+    column_index_map_name = {}
+    column_name_map_index = {}
+    table_rows = {} 
+    column_index = 0
+    for column in table.list_columns:
+        if column.name == pivot_column :
+            pivot_column_index = column_index
+        if column.name == value_column :
+            value_column_index = column_index
+        column_index_map_name[column_index] = column.name
+        column_name_map_index[column.name] = column_index
+        column_index = column_index  + 1
+    table_data_begin_row_index =  table.data_range.first_row 
+    table_data_end_row_index =  table.data_range.first_row  +  table.data_range.row_count
+    table_data_begin_column_index =  table.data_range.first_column  
+    table_data_end_column_index =  table.data_range.first_column  +  table.data_range.column_count
+
+    for row_index in range( table_data_begin_row_index, table_data_end_row_index):
+        cur_pivot_column_value = None
+        cur_value_column_value = None
+        cur_row = None
+        IsFirstCell = True
+        for column_index in range(table_data_begin_column_index ,table_data_end_column_index ):
+            if column_index == pivot_column_index:
+                cur_pivot_column_value = cells[row_index,column_index].value
+            elif column_index == value_column_index :
+                cur_value_column_value = cells[row_index,column_index].value
+            else:        
+                cell_value = cells[row_index,column_index].value
+                if IsFirstCell == True :                           
+                    if  cell_value in  table_rows:
+                        cur_row = table_rows[cell_value]
+                    else:
+                        table_rows[cell_value] ={}
+                        cur_row = table_rows[cell_value]
+                    IsFirstCell = False
+                else:
+                    if cell_value in cur_row :
+                        cur_row = cur_row[cell_value]
+                    else :
+                        cur_row[cell_value] ={}
+                        cur_row = cur_row[cell_value]                             
+                
+        cur_row[cur_pivot_column_value] = cur_value_column_value
+    return table_rows
+  
     
-    pass
--- SpreadsheetPandas/test/test_base_info.py
+++ SpreadsheetPandas/test/test_base_info.py
@@ -25,16 +25,87 @@ class TestBaseInfo( unittest.TestCase):
     
     def tearDown(self):
         pass
-    def test_base_info_table(self):
-        workbook = Workbook("/home/cells/Projects/cells-toolset/TestData/Input/BookTableData.xlsx")
-        print("===============")
-        list_object = workbook.worksheets[0].list_objects[0]
-        print (list_object.data_range.first_row)
-        print (list_object.end_row)
-        for row_index in range(list_object.data_range.first_row ,list_object.end_row ) :
-            print(row_index)
-        print("===============")
-        pass        
+
+    def test_list_unique(self):
+        header =["A","Product","Year", "Sales" ]
+        source = [["up","iphone","2012",122],["up","ipad","2012",122],["up","iphone","2013",122] ,["up","ipad","2013",120],["down","iphone","2012",122],["down","ipad","2012",122],["down","iphone","2013",122] ,["down","ipad","2013",120]]
+        source.insert(0,header)
+        print( source)
+        pass
+    # def test_base_info_table(self):
+    #     workbook = Workbook("/home/cells/Projects/cells-toolset/TestData/Input/BookTableData.xlsx")
+    #     print("===============")
+    #     list_object = workbook.worksheets[0].list_objects[0]
+    #     print (list_object.data_range.first_row)
+    #     print (list_object.end_row)
+    #     for row_index in range(list_object.data_range.first_row ,list_object.end_row ) :
+    #         print(row_index)
+    #     print("===============")
+    #     pass        
+
+    # def test_build_row_dict(self):
+    #     # rows ={"up":  { "ipad" : { "2015": 100 , "2016": 3000 } } }
+    #     source = [["up","iphone","2012",122],["up","ipad","2012",122],["up","iphone","2013",122] ,["up","ipad","2013",120],["down","iphone","2012",122],["down","ipad","2012",122],["down","iphone","2013",122] ,["down","ipad","2013",120]]
+        
+    #     # rows = {}
+    #     # for old_row in source:
+    #     #     cur_row = old_row
+    #     #     IsFirstCell = True
+    #     #     for cell in old_row :
+    #     #         if IsFirstCell : 
+    #     #             if cell in rows :
+    #     #                 cur_row = self.__build_row_(rows ,cell )
+    #     #             else :
+    #     #                 rows[cell] ={}
+    #     #                 cur_row = rows[cell]
+    #     #             IsFirstCell = False
+    #     #         else:
+    #     #             if cell in cur_row :
+    #     #                 cur_row = self.__build_row_(cur_row ,cell )
+    #     #             else :
+    #     #                 cur_row[cell] ={}
+    #     #                 cur_row = cur_row[cell]
+    #     # print(source)
+    #     # print(rows)
+
+    #     print("=======================================================================")       
+    #     rows = {} 
+    #     pivot_index = 2
+    #     value_index = 3 
+    #     column_index = 0
+    #     pivot_value = None
+    #     value_value = None
+    #     for old_row in source:
+    #         cur_row = old_row
+    #         IsFirstCell = True
+    #         column_index = 0
+    #         for cell in old_row :
+    #             if column_index == pivot_index :
+    #                 pivot_value = cell
+    #             elif column_index == value_index:
+    #                 value_value = cell
+    #             else:
+    #                 if IsFirstCell : 
+    #                     if cell in rows :
+    #                         cur_row = self.__build_row_(rows ,cell )
+    #                     else :
+    #                         rows[cell] ={}
+    #                         cur_row = rows[cell]
+    #                     IsFirstCell = False
+    #                 else:
+    #                     if cell in cur_row :
+    #                         cur_row = self.__build_row_(cur_row ,cell )
+    #                     else :
+    #                         cur_row[cell] ={}
+    #                         cur_row = cur_row[cell]              
+    #             column_index = column_index +1      
+    #         cur_row[pivot_value] = value_value
+    #     print(rows)            
+    #     pass
+        
+    def __build_row_(self, rows , key ) -> dict: 
+        return rows[key]
+    
 
 if __name__ == '__main__':
     unittest.main()
